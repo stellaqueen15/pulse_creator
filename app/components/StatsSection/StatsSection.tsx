0a2f2d7216e07950e383347cdad3c352a6967309
stellaqueen15 <brendamisenga@gmail.com>
2026-08-30 12:35:39 -0400

    Add StatsSection section

---
+++ app/components/StatsSection/StatsSection.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import StatCard from "@/app/components/StatsSection/StatCard";
+
+export default function StatsSection() {
+    const [stats, setStats] = useState(null);
+
+    useEffect(() => {
+        fetch("/api/dashboard/stats")
+            .then((res) => res.json())
+            .then((data) => setStats(data));
+    }, []);
+
+    const statCards = [
+        {
+            key: "totalAudience",
+            label: "Total Audience",
+            icon: "fi fi-sr-users",
+        },
+        {
+            key: "totalViews",
+            label: "Cumulative Views ",
+            period: "Last 30 Days",
+            icon: "fi fi-sr-eye",
+        },
+        {
+            key: "engagementRate",
+            label: "Average Engagement Rate",
+            icon: "fi fi-sr-heart",
+        },
+        {
+            key: "estimatedRevenue",
+            label: "Estimated Monthly Revenue",
+            icon: "fi fi-sr-dollar",
+        },
+    ];
+
+    return (
+        <div className="w-full p-6 flex gap-4">
+            {stats &&
+                statCards.map((card) => (
+                    <StatCard
+                        key={card.key}
+                        label={card.label}
+                        icon={card.icon}
+                        value={stats[card.key].value}
+                        change={stats[card.key].change}
+                        period={card.period}
+                    />
+                ))}
+        </div>
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,59 +1,14 @@
 "use client";
-
-import { useEffect, useState } from "react";
-import StatCard from "@/app/components/StatsSection/StatCard";
+import StatsSection from "@/app/components/StatsSection/StatsSection";
 
 export default function Home() {
-  const [stats, setStats] = useState(null);
-
-  useEffect(() => {
-    fetch("/api/dashboard/stats")
-        .then((res) => res.json())
-        .then((data) => setStats(data));
-  }, []);
-
-  const statCards = [
-    {
-      key: "totalAudience",
-      label: "Total Audience",
-      icon: "fi fi-sr-users",
-    },
-    {
-      key: "totalViews",
-      label: "Cumulative Views ",
-      period: "Last 30 Days",
-      icon: "fi fi-sr-eye",
-    },
-    {
-      key: "engagementRate",
-      label: "Average Engagement Rate",
-      icon: "fi fi-sr-heart",
-    },
-    {
-      key: "estimatedRevenue",
-      label: "Estimated Monthly Revenue",
-      icon: "fi fi-sr-dollar",
-    },
-  ];
 
   return (
       <div className="flex">
         <div className="bg-red-500 h-screen fixed w-60"></div>
         <div className="ml-60 w-full">
           <div className="bg-green-500 h-16 w-full"></div>
-          <div className="w-full p-6 flex gap-4">
-            {stats &&
-                statCards.map((card) => (
-                    <StatCard
-                        key={card.key}
-                        label={card.label}
-                        icon={card.icon}
-                        value={stats[card.key].value}
-                        change={stats[card.key].change}
-                        period={card.period}
-                    />
-                ))}
-          </div>
+          <StatsSection />
           <div className="bg-pink-600 h-72 w-full"></div>
           <div className="bg-purple-500 h-71 w-full"></div>
         </div>
