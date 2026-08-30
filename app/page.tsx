8b32757e3d282c51d2e02d9d2ed870eae256c41e
stellaqueen15 <brendamisenga@gmail.com>
2026-08-29 22:02:11 -0400

    add structure

--- app/page.tsx
+++ app/page.tsx
@@ -12,6 +12,14 @@ export default function Home() {
   }, []);
 
   return (
-      <pre>{JSON.stringify(stats, null, 2)}</pre>
+      <div className="bg-black flex">
+        <div className="bg-red-500 h-screen fixed w-60"></div>
+        <div className="bg-yellow-500 w-full">
+          <div className="bg-green-500 h-16 w-full"></div>
+          <div className="bg-blue-500 h-24 w-full"></div>
+          <div className="bg-pink-600 h-72 w-full"></div>
+          <div className="bg-purple-500 h-71 w-full"></div>
+        </div>
+      </div>
   );
 }
