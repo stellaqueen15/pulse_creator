06c617b13cbb8b8b100b7d3e7723ca873111e962
stellaqueen15 <brendamisenga@gmail.com>
2026-08-30 16:55:06 -0400

    fix style

--- app/components/Topbar/Topbar.tsx
+++ app/components/Topbar/Topbar.tsx
@@ -19,13 +19,13 @@ export default function Topbar() {
                     <i className="fi fi-br-brightness flex items-center text-xl"></i>
                 </button>
 
-                <button className="relative flex h-10 w-10 items-center justify-center rounded-xl bg-white/10 text-white transition hover:bg-white/15">
+                <button className="relative flex h-10 w-10 items-center justify-center rounded-xl cursor-pointer bg-white/10 text-white transition hover:bg-white/15">
                     <i className="fi fi-ss-bell flex items-center text-xl"></i>
 
                     <span className="absolute right-3 top-2 h-1.5 w-1.5 rounded-full bg-red-500"></span>
                 </button>
 
-                <div className="ml-2 flex items-center gap-3 border-l border-white/10 pl-4">
+                <div className="ml-2 flex items-center gap-3 border-l border-white/10 pl-4 cursor-pointer">
                     <div className="flex h-9 w-9 items-center justify-center overflow-hidden rounded-full bg-white/20">
                         <img
                             src="https://i.pravatar.cc/100?img=47"
