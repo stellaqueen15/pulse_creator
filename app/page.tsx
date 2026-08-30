1509d514e64e793c6884b2edd02b4d320d366c26
stellaqueen15 <brendamisenga@gmail.com>
2026-08-29 21:58:20 -0400

    test msw in home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,17 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 export default function Home() {
+  const [stats, setStats] = useState(null);
+
+  useEffect(() => {
+    fetch("/api/dashboard/stats")
+        .then((res) => res.json())
+        .then((data) => setStats(data));
+  }, []);
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-    </div>
+      <pre>{JSON.stringify(stats, null, 2)}</pre>
   );
 }
